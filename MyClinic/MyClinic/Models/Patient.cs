b7fb3e014f01adb634cebb2abee5549e7aa983c8
Qfernandes <114498791+Qfernandes@users.noreply.github.com>
2024-01-10 11:57:31 +0000

    Patient.cs
    Created contact number and email address
    Added validation for email address and contact number

--- MyClinic/MyClinic/Models/Patient.cs
+++ MyClinic/MyClinic/Models/Patient.cs
@@ -25,7 +25,22 @@ namespace MyClinic.Models
         [StringLength(100, ErrorMessage = "Invalid Gender")]
         [Display(Name = "Gender")]
         public string Gender { get; set; }
+        
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        [Phone]
+        [StringLength(10, ErrorMessage = "Phone number cannot be longer than 10 characters.")]
+        [Display(Name = "ContactNumber")]
+        public string ContactNumber { get; set; }
 
+
+
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "Address cannot be longer than 100 characters.")]
