fba418bea6da0e09aac5f1fad5a91d2ccaa39e75
ravleen29 <114485306+ravleen29@users.noreply.github.com>
2024-01-10 11:45:12 +0000

    treatment 2

--- MyClinic/MyClinic/Models/Treatment.cs
+++ MyClinic/MyClinic/Models/Treatment.cs
@@ -1,6 +1,12 @@
-﻿namespace MyClinic.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyClinic.Models
 {
     public class Treatment
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int TreatmentID { get; set; }
+        public string TreatmentName { get; set; }
+        public int Price { get; set; }
     }
 }
