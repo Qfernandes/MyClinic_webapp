419ab1993a96a3840b52882545e4f4521991245f
ravleen29 <114485306+ravleen29@users.noreply.github.com>
2024-01-10 11:49:52 +0000

    removing errors

--- MyClinic/MyClinic/Pages/Error.cshtml.cs
+++ MyClinic/MyClinic/Pages/Error.cshtml.cs
@@ -8,7 +8,7 @@ namespace MyClinic.Pages
     [IgnoreAntiforgeryToken]
     public class ErrorModel : PageModel
     {
-        public string? RequestId { get; set; }
+        public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
