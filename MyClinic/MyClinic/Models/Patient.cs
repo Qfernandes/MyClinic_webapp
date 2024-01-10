7866403ca20ceb199aac64bd5630902b0c5db39b
Qfernandes <114498791+Qfernandes@users.noreply.github.com>
2024-01-10 11:56:11 +0000

    Patient.cs
    Created patient.cs
    Creaed patient attributes

---
+++ MyClinic/MyClinic/Models/Patient.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using MyClinic.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyClinic.Models
+{
+    public class Patient
+    {
+        public int PatientID { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Full Name cannot be longer than 100 characters.")]
+        [Column("FullName")]
+        [Display(Name = " Patient Name")]
+        public string FullName { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "DOB ")]
+        public DateTime DOB { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Invalid Gender")]
+        [Display(Name = "Gender")]
+        public string Gender { get; set; }
+
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Address cannot be longer than 100 characters.")]
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Next of Kin cannot be longer than 100 characters.")]
+        [Display(Name = "NextOfKin")]
+        public string NextOfKin { get; set; }
+        public string MedicalHistory { get; set; }
+
+
+
+
+
+    }
+
+}
