7d4905c4bd7745d8efbd25c0e11dcf557bd99473
Sakthy1 <114680260+Sakthy1@users.noreply.github.com>
2024-01-11 15:55:53 +0000

    schedule creation

---
+++ MyClinic/MyClinic/Models/Schedule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Schedule
+{
+	public Schedule
+	{
+        public int ScheduleID { get; set; }
+        public DateTime DateTime { get; set; }
+        public TreatmentpatientID { get; set; }
+        public PatientID 
+        public PaymentID
+        public AssistantID
+
+        public Assistant Assistant { get; set; }
+        public Patient Patient { get; set; }
+        public Payment Payment { get; set; }
+        public Treatmentpatient Treatmentpatient { get; set; }
+
+    }
+	
+}
--- MyClinic/MyClinic/obj/Release/net6.0/MyClinic.AssemblyInfo.cs
+++ MyClinic/MyClinic/obj/Release/net6.0/MyClinic.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-MyClinic-ed365131-eb3b-4c2c-883e-c4401bdb4648")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyClinic")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyClinic")]
