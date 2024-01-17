ef88fce70d19f6fce5626ee57892667914aa330a
Qfernandes <114498791+Qfernandes@users.noreply.github.com>
2024-01-17 10:29:16 +0000

    Created Assistant class

---
+++ MyClinic/MyClinic/Models/Assistant.cs
@@ -0,0 +1,29 @@
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyClinic.Models
+{
+    public class Assistant
+    {
+        public int AssistantID { get; set; }
+
+        [Required]
+        [Display(Name = "LastName")]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [Column("FirstMidName")]
+        [Display(Name = "Assistant Name")]
+        [StringLength(50)]
+        public string FirstMidName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "DOB")]
+        public DateTime DOB { get; set; }
+
+        public ICollection<Schedule> Schedules { get; set; }
+    }
+}
