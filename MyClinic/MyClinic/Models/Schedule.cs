8ed5dea94e857430e45250d69447915884b2df88
Qfernandes <114498791+Qfernandes@users.noreply.github.com>
2024-01-17 10:29:50 +0000

    Updated Schedule class

--- MyClinic/MyClinic/Models/Schedule.cs
+++ MyClinic/MyClinic/Models/Schedule.cs
@@ -1,22 +1,31 @@
 ﻿using System;
 
-/*public class Schedule
+/*namespace MyClinic.Models
 {
-	public Schedule
-	{
+    public class Schedule
+    {
         public int ScheduleID { get; set; }
-        public DateTime DateTime { get; set; }
-        public TreatmentpatientID { get; set; }
-        public PatientID 
-        public PaymentID
-        public AssistantID
 
+        public DateTime DateTime { get; set; }
+        public Service? Service { get; set; }
+
+        public PaymentStatus? Paymentstatus { get; set; }
+
+        public Treatmentpatient Treatmentpatient { get; set; }
+        //public Treatment Treatment { get; set; }
         public Assistant Assistant { get; set; }
         public Patient Patient { get; set; }
-        public Payment Payment { get; set; }
-        public Treatmentpatient Treatmentpatient { get; set; }
 
+        public Payment Payment { get; set; }
+
+
+        public int TreatmentpatientID { get; set; }
+        public int PatientID { get; set; }
+        public int PaymentID { get; set; }
+        public int AssistantID { get; set;}
     }
-	
 }
 */
+
+        
+    
