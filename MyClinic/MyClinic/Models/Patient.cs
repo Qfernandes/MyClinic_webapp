31d95c0c9f8be7fba2abd83d0448aa3df4e22a33
Qfernandes <114498791+Qfernandes@users.noreply.github.com>
2024-01-18 09:23:09 +0000

    Updated Patient.cs

--- MyClinic/MyClinic/Models/Patient.cs
+++ MyClinic/MyClinic/Models/Patient.cs
@@ -26,23 +26,20 @@ namespace MyClinic.Models
         [Display(Name = "Gender")]
         public string Gender { get; set; }
 
-        /*
+        
         [Required]
         [DataType(DataType.PhoneNumber)]
         [Phone]
         [StringLength(10, ErrorMessage = "Phone number cannot be longer than 10 characters.")]
         [Display(Name = "ContactNumber")]
-        */
+        
         public string ContactNumber { get; set; }
-
-
-
-        /*
+        
         [Required]
         [DataType(DataType.EmailAddress)]
         [EmailAddress]
         [Display(Name = "Email")]
-        */
+        
         public string Email { get; set; }
 
         [Required]
