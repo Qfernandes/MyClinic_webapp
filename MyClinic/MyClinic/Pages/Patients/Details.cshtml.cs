1c07609b5941dfce3faf448f7312ba2dacfc583c
Qfernandes <114498791+Qfernandes@users.noreply.github.com>
2024-01-18 09:26:00 +0000

    Updated Patient details html and html.cs

---
+++ MyClinic/MyClinic/Pages/Patients/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MyClinic.Data;
+using MyClinic.Models;
+
+namespace MyClinic.Pages.Patients
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly MyClinic.Data.MyClinicContext _context;
+
+        public DetailsModel(MyClinic.Data.MyClinicContext context)
+        {
+            _context = context;
+        }
+
+        public Patient Patient { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Patients == null)
+            {
+                return NotFound();
+            }
+
+            Patient = await _context.Patients
+                    .Include(s => s.Treatmentpatients)
+                    .Include(e => e.Payments)
+                    .AsNoTracking()
+                    .FirstOrDefaultAsync(m => m.PatientID == id);
+            if (Patient == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
