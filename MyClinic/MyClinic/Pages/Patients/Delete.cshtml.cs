203e89714cdbd2e70308e9962e724f30dff0a540
Qfernandes <114498791+Qfernandes@users.noreply.github.com>
2024-01-18 09:25:39 +0000

    Updated Patient delete html and html.cs

---
+++ MyClinic/MyClinic/Pages/Patients/Delete.cshtml.cs
@@ -0,0 +1,80 @@
+﻿using MyClinic.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace MyClinic.Pages.Patients
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly MyClinic.Data.MyClinicContext _context;
+        private readonly ILogger<DeleteModel> _logger;
+
+        public DeleteModel(MyClinic.Data.MyClinicContext context,
+                           ILogger<DeleteModel> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [BindProperty]
+        public Patient Patient { get; set; }
+        public string ErrorMessage { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id, bool? saveChangesError = false)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Patient = await _context.Patients
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.PatientID == id);
+
+            if (Patient == null)
+            {
+                return NotFound();
+            }
+
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ErrorMessage = String.Format("Delete {ID} failed. Try again", id);
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var patient = await _context.Patients.FindAsync(id);
+
+            if (patient == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                _context.Patients.Remove(patient);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
+            }
+            catch (DbUpdateException ex)
+            {
+                _logger.LogError(ex, ErrorMessage);
+
+                return RedirectToAction("./Delete",
+                                     new { id, saveChangesError = true });
+            }
+        }
+    }
+}
