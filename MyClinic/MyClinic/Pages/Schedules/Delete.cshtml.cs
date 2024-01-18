48e816e8cb805ecca06edb620d06dbbc4679f7f0
Qfernandes <114498791+Qfernandes@users.noreply.github.com>
2024-01-18 09:26:59 +0000

    Updated Schedule delete html and html.cs

---
+++ MyClinic/MyClinic/Pages/Schedules/Delete.cshtml.cs
@@ -0,0 +1,80 @@
+﻿using MyClinic.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace MyClinic.Pages.Schedules
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly MyClinic.Data.MyClinicContext _context;
+        private readonly ILogger<DeleteModel> _logger;
+
+        public DeleteModel(MyClinic.Data.MyClinicContext context,
+                           ILogger<DeleteModel> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [BindProperty]
+        public Schedule Schedule { get; set; }
+        public string ErrorMessage { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id, bool? saveChangesError = false)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Schedule = await _context.Schedules
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.ScheduleID == id);
+
+            if (Schedule == null)
+            {
+                return NotFound();
+            }
+
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ErrorMessage = String.Format("Delete {ID} failed. Try again", id);
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var schedule = await _context.Schedules.FindAsync(id);
+
+            if (schedule == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                _context.Schedules.Remove(schedule);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
+            }
+            catch (DbUpdateException ex)
+            {
+                _logger.LogError(ex, ErrorMessage);
+
+                return RedirectToAction("./Delete",
+                                     new { id, saveChangesError = true });
+            }
+        }
+    }
+}
