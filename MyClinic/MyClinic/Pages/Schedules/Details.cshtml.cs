f2716029929d07d79281ffaed0cd2c92fd5ff843
Qfernandes <114498791+Qfernandes@users.noreply.github.com>
2024-01-18 09:27:10 +0000

    Updated Schedule details html and html.cs

---
+++ MyClinic/MyClinic/Pages/Schedules/Details.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MyClinic.Data;
+using MyClinic.Models;
+
+namespace MyClinic.Pages.Schedules
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly MyClinic.Data.MyClinicContext _context;
+
+        public DetailsModel(MyClinic.Data.MyClinicContext context)
+        {
+            _context = context;
+        }
+
+        public Schedule Schedule { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Schedules == null)
+            {
+                return NotFound();
+            }
+
+            Schedule = await _context.Schedules
+                    .Include(e => e.Patient)
+                    .Include(f => f.Assistant)
+                    .AsNoTracking()
+                    .FirstOrDefaultAsync(m => m.ScheduleID == id);
+
+            if (Schedule == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
