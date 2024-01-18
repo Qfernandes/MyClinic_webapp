b20b49a05c87241ac738d151c71f612ce0e1aba7
Qfernandes <114498791+Qfernandes@users.noreply.github.com>
2024-01-18 09:26:49 +0000

    Updated Schedule create html and html.cs

---
+++ MyClinic/MyClinic/Pages/Schedules/Create.cshtml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MyClinic.Data;
+using MyClinic.Models;
+
+namespace MyClinic.Pages.Schedules
+{
+    public class CreateModel : PageModel
+    {
+        private readonly MyClinic.Data.MyClinicContext _context;
+
+        public CreateModel(MyClinic.Data.MyClinicContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Schedule Schedule { get; set; }
+
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var emptySchedule = new Schedule();
+
+            if (await TryUpdateModelAsync<Schedule>(
+                emptySchedule,
+                "schedule",   // Prefix for form value.
+                s => s.DateTime))
+            {
+                _context.Schedules.Add(emptySchedule);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
+            }
+
+            return Page();
+        }
+    }
+}
